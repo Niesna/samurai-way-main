d2f261a4275c0714e1055b8b478cc56e7d90f794
Niesna <4february1221@gmail.com>
2023-04-26 20:52:17 +0300

    пропсы.

--- src/Technologies.tsx
+++ src/Technologies.tsx
@@ -2,14 +2,27 @@ import React from "react";
 
 const Technologies = () => {
     return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    );
+        <ul>
+            <UnList />
+        </ul>
+    )
+}
+
+const UnList = () => {
+            return (
+                <ul>
+                <List1 value={"CSS"}/>
+                <List1 value={"HTML"}/>
+                <List1 value={"JS"}/>
+                <List1 value={"React"}/>
+                </ul>
+            )
+}
+
+type List1PropsType = {
+    value: string
+}
+function List1  (props: List1PropsType) {
+    return <li>{props.value}</li>
 }
 export default Technologies
